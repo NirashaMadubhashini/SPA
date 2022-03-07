96c93e8929d1e81a0fec51dd181a4f710e8ab802
NirashaMadubhashini <nirashamadu0729@gmail.com>
2022-03-07 13:28:58 +0530

    update CustomerDTO.js

--- model/CustomerDTO.js
+++ model/CustomerDTO.js
@@ -4,4 +4,34 @@ function CustomerDTO(id,name,address,salary) {
     var __address=address;
     var __salary=salary;
 
+    this.getId = function () {
+        return __id;
+    }
+    this.setId = function (v) {
+        __id = v;
+    }
+    this.getName = function () {
+        return __name;
+    }
+    this.setName = function (v) {
+        __name = v;
+    }
+    this.getAddress = function () {
+        return __address;
+    }
+    this.setAddress = function (v) {
+        __address = v;
+    }
+    this.getSalary = function () {
+        return __salary;
+    }
+    this.setSalary = function (v) {
+        __salary = v;
+    }
+
 }
+
+// var cc = new CustomerDTO("C001", "Dasun", "Galle", 1000);
+// cc.id; // not allowed
+// cc.setCustomerID("C004"); // ok
+// cc.getCustomerID(); // ok
