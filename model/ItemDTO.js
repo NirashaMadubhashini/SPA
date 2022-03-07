062b6aac79fec66f5c1da2a6b26f6b6e2b1e9dcc
NirashaMadubhashini <nirashamadu0729@gmail.com>
2022-03-07 13:29:41 +0530

    update ItemDTO.js

--- model/ItemDTO.js
+++ model/ItemDTO.js
@@ -1,7 +1,32 @@
 function ItemDTO(code,name,price,quantity) {
     var __code=code;
-    var __name=name;
+    var __itemName=name;
     var __price=price;
     var __quantity=quantity;
+
+    this.getCode = function () {
+        return __code;
+    }
+    this.setCode = function (v) {
+        __code = v;
+    }
+    this.getItemName = function () {
+        return __itemName;
+    }
+    this.setItemName = function (v) {
+        __itemName = v;
+    }
+    this.getPrice = function () {
+        return __price;
+    }
+    this.setPrice = function (v) {
+        __price = v;
+    }
+    this.getQuantity = function () {
+        return __quantity;
+    }
+    this.setQuantity = function (v) {
+        __quantity = v;
+    }
 
 }
