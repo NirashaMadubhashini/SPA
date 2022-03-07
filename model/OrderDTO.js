0185bb748e0920083d8203969b6c0f09e10aa582
NirashaMadubhashini <nirashamadu0729@gmail.com>
2022-03-07 13:30:03 +0530

    update OrderDTO.js

---
+++ model/OrderDTO.js
@@ -0,0 +1,58 @@
+function OrderDTO(orderId,itemCode,itemName,qty,price,total) {
+var __orderId=orderId;
+var __itemCode=itemCode;
+var __itemName=itemName;
+var __qty=qty;
+var __price=price;
+var __total=total;
+
+this.getOrderId=function () {
+return __orderId;
+}
+this.setOrderId=function () {
+return __orderId;
+}
+
+this.getItemCode=function () {
+return __itemCode;
+}
+this.setItemCode=function () {
+return __itemCode;
+}
+
+this.getItemName=function () {
+return __itemName;
+}
+this.setItemName=function () {
+return __itemName;
+}
+
+this.getQty=function () {
+return __qty;
+}
+this.setQty=function () {
+return __qty;
+}
+
+this.getPrice=function () {
+return __price;
+}
+this.setPrice=function () {
+return __price;
+}
+
+this.getTotal=function () {
+return __total;
+}
+this.setTotal=function () {
+return __total;
+}
+
+
+}
+
+
+
+
+
+
