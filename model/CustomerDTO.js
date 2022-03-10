e7d61cf3e900530339a2a5306f6e875482d1f1b6
NirashaMadubhashini <nirashamadu0729@gmail.com>
2022-03-10 15:24:03 +0530

    update CustomerDTO.js

--- model/CustomerDTO.js
+++ model/CustomerDTO.js
@@ -1,37 +1,55 @@
-function CustomerDTO(customerId,customerName,customerAddress,salary) {
-    var __customerId=customerId;
-    var __customerName=customerName;
-    var __customerAddress=customerAddress;
-    var __salary=salary;
+function CustomerDTO(cid,cname,caddress,csalary) {
 
-    this.getCustomerId = function () {
-        return __customerId;
-    }
-    this.setCustomerId = function (v) {
-        __customerId = v;
-    }
-    this.getCustomerName = function () {
-        return __customerName;
-    }
-    this.setCustomerName = function (v) {
-        __customerName = v;
-    }
-    this.getCustomerAddress = function () {
-        return __customerAddress;
-    }
-    this.setCustomerAddress = function (v) {
-        __customerAddress = v;
-    }
-    this.getCustomerSalary= function () {
-        return __customerSalary;
-    }
-    this.setCustomerSalary = function (v) {
-        __customerSalary = v;
-    }
+    var id =cid;
+    var name=cname;
+    var address=caddress;
+    var salary=csalary;
 
+
+    Object.defineProperty(this,"cid",{
+        get:function()
+        {
+            return id;
+        },
+        set:function(cid)
+        {
+            this.id=cid;
+        }
+    });
+
+
+    Object.defineProperty(this,"cname",{
+        get:function()
+        {
+            return name;
+        },
+        set:function(cname)
+        {
+            this.name=cname;
+        }
+    });
+
+
+    Object.defineProperty(this,"caddress",{
+        get:function()
+        {
+            return address;
+        },
+        set:function(caddress)
+        {
+            this.address=caddress;
+        }
+    });
+
+
+    Object.defineProperty(this,"csalary",{
+        get:function()
+        {
+            return salary;
+        },
+        set:function(csalary)
+        {
+            this.salary=csalary;
+        }
+    });
 }
-
-// var cc = new CustomerDTO("C001", "Dasun", "Galle", 1000);
-// cc.id; // not allowed
-// cc.setCustomerID("C004"); // ok
-// cc.getCustomerID(); // ok
