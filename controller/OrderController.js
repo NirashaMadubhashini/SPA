dc2a04815ef740789250f4c4ac650314f953bd76
NirashaMadubhashini <nirashamadu0729@gmail.com>
2022-03-12 17:03:00 +0530

    update OrderController.js

--- controller/OrderController.js
+++ controller/OrderController.js
@@ -12,7 +12,7 @@ $("#btnAddToCart").click(function () {
 function loadAllOrders() {
     $("#orderTable").empty();
     for (var i of orderDB) {
-        let row = `<tr><td>${i.orderId}</td><td>${Array(item).itemCode}</td><td>${i.qty}</td><td>${i.unitPrice}</td><td>${i.total}</td></tr>`;
+        let row = `<tr><td>${i.orderId}</td><td>${i.itemCode}</td><td>${i.itemName}</td><td>${i.qty}</td><td>${i.price}</td><td>${i.total}</td></tr>`;
         $("#orderTable").append(row);
     }
 }
@@ -26,40 +26,29 @@ function saveOrder() {
     } else {
         confirm("Do you want to add this Order..?")
 
-    let orderDTO = new OrderDTO(
-        $("#txtOrderId").val(),
-        $("#selectCusID").val(),
-        $("#selectItemCode").val(),
-        $("#txt-qty").val(),
-        $("#total").val());
+        let orderDetailDTO = new OrderDetailDTO(
+            $("#txtOrderId").val(),
+            $('#selectCusID').icode,
+            $("#txtItemName").val(),
+            $("#txt-qty").val(),
+            $("#txtUnitPrice").val(),
+            $("#total").val()
+            )
+        ;
 
 
-    var orderObject = {
-        orderId: orderDTO.oid,
-        customerId: orderDTO.cid,
-        itemCode: orderDTO.icode,
-        qtOH: orderDTO.qtyOnHnd,
-        tot: orderDTO.cost
-    }
-
-    orderDB.push(orderObject)
-}
-}
-
-$("#btnRemove").click(function () {
-    var index = 0;
-    for (var i = 0; i < orderDB.length; i++) {
-        if ($("#txtOrderId").val() === orderDB[i].orderId) {
-            index = i;
+        var orderObject = {
+            orderId: orderDetailDTO.orderid,
+            itemCode: orderDetailDTO.itcode,
+            itemName: orderDetailDTO.itname,
+            qty: orderDetailDTO.itqty,
+            price: orderDetailDTO.itprice,
+            total: orderDetailDTO.ittot
         }
+
+        orderDB.push(orderObject)
     }
-    orderDB.splice(index, 1);
-    clearAllOrder();
-    $(this).closest('tr').remove();
-
-    confirm("Do you want to delete this Order..?")
-});
-
+}
 
 
 function duplicateCheckOrderId() {
@@ -73,28 +62,37 @@ function duplicateCheckOrderId() {
 }
 
 
-
-
-
-
 $('#selectCusID').change(function () {
-    var customerId = $('#selectCusID').val();
+    var customerId = $('#selectCusID').find(":selected").text();
     for (let i = 0; i < customerDB.length; i++) {
-        if (customerDB[i].getCustomerId() == customerId) {
-            $('#txtCusName').val(customerDB[i].getCustomerName());
-            $('#txtAddress').val(customerDB[i].getCustomerAddress());
-            $('#txtSalary').val(customerDB[i].getCustomerSalary());
+        console.log(customerDB[0].id)
+        if (customerDB[i].id === customerId) {
+            console.log(customerDB[i])
+            console.log(customerDB[i].cname)
+            $('#txtCusName').val(customerDB[i].name);
+            $('#txtAddress').val(customerDB[i].address);
+            $('#txtSalary').val(customerDB[i].salary);
+        }
+    }
+});
+
+$('#selectItemCode').change(function () {
+    var itemCode = $('#selectItemCode').find(":selected").text();
+    for (let i = 0; i < itemDB.length; i++) {
+        console.log(itemDB[0].itemCode)
+        if (itemDB[i].itemCode === itemCode) {
+            console.log(itemDB[i])
+            console.log(itemDB[i].cname)
+            $('#txtItemName').val(itemDB[i].itemName);
+            $('#txtUnitPrice').val(itemDB[i].price);
+            $('#txt-qtyOnHand').val(itemDB[i].quantity);
         }
     }
 });
 
 
-
-
-
-
-const orderIDRegEx=/^(O00-)[0-9]{3}$/;
-const qtOHRegEx = /^[0-9 A-z]{2,}$/;
+const orderIDRegEx = /^(O-)[0-9]{3}$/;
+const qtOHRegEx = /^[0-9]{2,}$/;
 const cashRegEx = /^[0-9]{2,}$/;
 
 
@@ -126,13 +124,28 @@ $("#txtOrderId").on('keyup', function (eventOb) {
 });
 
 
-
 $("#txt-qty").on('keyup', function (eventOb) {
     setButtonOrder();
     if (eventOb.key === "Enter") {
         checkIfValidOrder();
     }
 });
+
+$("#txt-qty").on('keydown', (function (e) {
+        let qtyOnHand = $("#txt-qtyOnHand").val();
+        let qtyTyped = $("#txt-qty").val() + "0";
+        let qtyOnHandNumber = parseInt(qtyOnHand);
+        let qtyNumber = parseInt(qtyTyped);
+        console.log(qtyTyped)
+        if (qtyOnHandNumber < qtyNumber) {
+            $("#txt-qty").css('border', '2px solid red');
+            $("#lblorderQty").text("insufficient Quantity");
+        } else {
+            $("#txt-qty").css('border', '2px solid green');
+            $("#lblorderQty").text("");
+        }
+    }
+));
 
 $("#txtCash").on('keyup', function (eventOb) {
     setButtonOrder();
@@ -143,7 +156,7 @@ $("#txtCash").on('keyup', function (eventOb) {
 
 $("#btnPay").attr('disabled', true);
 
-function clearAllOrder(){
+function clearAllOrder() {
     $('#txtOrderId,#txt-qty,#txtCash').val("");
     $('#txtOrderId,#txt-qty,#txtCash').css('border', '2px solid #ced4da');
     $('#txtOrderId').focus();
@@ -161,19 +174,19 @@ function formValidOrder() {
         if (qtOHRegEx.test(qty)) {
             $("#txt-qty").css('border', '2px solid green');
             $("#lblcusname").text("");
-                var cash = $("#txtCash").val();
-                var resp = cashRegEx.test(cash);
-                $("#txt-qty").css('border', '2px solid green');
-                $("#lblorderQty").text("");
-                if (resp) {
-                    $("#txtCash").css('border', '2px solid green');
-                    $("#lblcash").text("");
-                    return true;
-                } else {
-                    $("#txtCash").css('border', '2px solid red');
-                    $("#lblcash").text("Insufficient cash balance");
-                    return false;
-                }
+            var cash = $("#txtCash").val();
+            var resp = cashRegEx.test(cash);
+            $("#txt-qty").css('border', '2px solid green');
+            $("#lblorderQty").text("");
+            if (resp) {
+                $("#txtCash").css('border', '2px solid green');
+                $("#lblcash").text("");
+                return true;
+            } else {
+                $("#txtCash").css('border', '2px solid red');
+                $("#lblcash").text("Insufficient cash balance");
+                return false;
+            }
         } else {
             $("#txt-qty").css('border', '2px solid red');
             $("#lblorderQty").text("insufficient Quantity");
@@ -181,30 +194,30 @@ function formValidOrder() {
         }
     } else {
         $("#txtOrderId").css('border', '2px solid red');
-        $("#lblorderId").text("Order ID is a required field : Pattern O00-000");
+        $("#lblorderId").text("Order ID is a required field : Pattern O-000");
         return false;
     }
 }
 
 
-function checkIfValidOrder(){
+function checkIfValidOrder() {
     var orderID = $("#txtOrderId").val();
     if (orderIDRegEx.test(orderID)) {
         $("#txt-qty").focus();
         var qty = $("#txt-qty").val();
         if (qtOHRegEx.test(qty)) {
-                $("#txtCash").focus();
-                var cash = $("#txtCash").val();
-                var resp = cashRegEx.test(cash);
-                if (resp) {
-                    let res = confirm("Do you really want to Purchase this order..?");
-                    if (res) {
-                        saveOrder();
-                        clearAllOrder();
-                    }
-                } else {
-                    $("#txtCash").focus();
+            $("#txtCash").focus();
+            var cash = $("#txtCash").val();
+            var resp = cashRegEx.test(cash);
+            if (resp) {
+                let res = confirm("Do you really want to Purchase this order..?");
+                if (res) {
+                    saveOrder();
+                    clearAllOrder();
                 }
+            } else {
+                $("#txtCash").focus();
+            }
         } else {
             $("#txt-qty").focus();
         }
@@ -213,7 +226,7 @@ function checkIfValidOrder(){
     }
 }
 
-function setButtonOrder(){
+function setButtonOrder() {
     let b = formValidOrder();
     if (b) {
         $("#btnPay").attr('disabled', false);
